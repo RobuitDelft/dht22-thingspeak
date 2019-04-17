cd34bf570225ca4282a33d102885693fb43248b1
Rob van Laarhoven <r.a.g.vanlaarhoven@tudelft.nl>
2019-04-17 22:46:59 +0200

    WLAN defenition in dict, so we can loop through tem while connecting

--- boot.py
+++ boot.py
@@ -4,21 +4,34 @@ import gc
 import webrepl
 import network
 import config
+import time
 
 webrepl.start()
 gc.collect()
 
-def do_connect_wlan():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.disconnect()
-    if not sta_if.isconnected():
-        print('connecting to WLAN...' + config.WLAN_SSID)
-        sta_if.active(True)
-        sta_if.connect(config.WLAN_SSID, config.WLAN_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
+# Connect to WIFI
+def connect_to_wifi():
+    nic = network.WLAN(network.STA_IF)
+    nic.active(True)
+    for x in range(len(config.wlandict)):
+        print('connecting to network: %s' % config.wlandict[x][0])
+        nic.connect(config.wlandict[x][0],config.wlandict[x][1])
+        # Wait some time
+        time.sleep(2.0)
+        attempt =  1
+        while attempt < 5 and not nic.isconnected():
+            print('connecting ... %s' % str(attempt*2))
+            time.sleep(2.0)
+            attempt = attempt + 1
+
+        if nic.isconnected():
+            print('connected')
+            print(nic.ifconfig()[0])
+            return True
+        
+    print('connection failed')
+    return False
+
 
 # Deactivate Access Point (to not annoy network guys)
 def activate_ap():
@@ -30,7 +43,7 @@ def deactivate_ap():
     ap_if.active(False)
 
 deactivate_ap()
-do_connect_wlan()
+connect_to_wifi()
 
 print("Load DHT22 module and Run")
 import dht22
--- dht22.py
+++ dht22.py
@@ -6,6 +6,7 @@ from os import urandom
 from umqtt.robust import MQTTClient
 from dht import DHT22
 import config
+import network
 
 try:
     import usocket as socket
@@ -14,33 +15,30 @@ except:
 
 import ussl as ssl
 
-## INIT SECTION
+# Connect to WIFI
+def connect_to_wifi():
+    # Try all wlans in config.wlandict
+    for x in range(len(config.wlandict)):
+        print('connecting to network: %s' % config.wlandict[x][0])
+        nic.connect(config.wlandict[x][0],config.wlandict[x][1])
+        # Wait some time
+        time.sleep(2.0)
+        attempt =  1
+        while attempt < 5 and not nic.isconnected():
+            print('connecting ... %s' % str(attempt*2))
+            time.sleep(2.0)
+            attempt = attempt + 1
 
-# create a random MQTT clientID & MQTTClient object
-randomNum = int.from_bytes(urandom(3), 'little')
-myMqttClient = bytes("client_"+str(randomNum), 'utf-8')
-client = MQTTClient(client_id=myMqttClient, 
-                    server=config.THINGSPEAK_URL, 
-                    user=config.THINGSPEAK_USER_ID, 
-                    password=config.THINGSPEAK_MQTT_KEY, 
-                    ssl=False)
+        if nic.isconnected():
+            print('connected')
+            print(nic.ifconfig()[0])
+            return True
+            lcd.putstr("WLAN:% \n IP:%s" % config.wlandict[x][0], nic.ifconfig()[0])
+            time.sleep(2.0)
+        
+    print('connection failed')
+    return False
 
-# Init LCD screen and set backlight off
-i2c = I2C(scl=Pin(config.LCD_PIN_SCL), sda=Pin(config.LCD_PIN_SDA), freq=400000)
-lcd = I2cLcd(i2c, 0x27, 2, 16)
-lcd.backlight_off()
-
-# Connect to TU Visitor WLAN
-def do_connect_WLAN():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(config.WLAN_SSID,config.WLAN_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-        print('network config:', sta_if.ifconfig())
 
 # measures temperature and humidity with DHT22 sensor, and sends the data to ThingSpeak, Terminal, LED and LCD
 def measure_temperature_and_humidity():
@@ -81,19 +79,50 @@ def measure_temperature_and_humidity():
     if h < 70:
        led(0)
 
+
+## INIT SECTION
+
+# Network interface
+nic = network.WLAN(network.STA_IF)
+nic.active(True)
+
+# create a random MQTT clientID & MQTTClient object
+randomNum = int.from_bytes(urandom(3), 'little')
+myMqttClient = bytes("client_"+str(randomNum), 'utf-8')
+client = MQTTClient(client_id=myMqttClient, 
+                    server=config.THINGSPEAK_URL, 
+                    user=config.THINGSPEAK_USER_ID, 
+                    password=config.THINGSPEAK_MQTT_KEY, 
+                    ssl=False)
+
+# Init LCD screen and set backlight off
+i2c = I2C(scl=Pin(config.LCD_PIN_SCL), sda=Pin(config.LCD_PIN_SDA), freq=400000)
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+lcd.backlight_off()
+
 ## RUN SECTION
 
 # (Re)Connect to TU Visitor WLAN
-do_connect_WLAN()
+if not nic.isconnected():
+    connect_to_wifi()
+lcd.clear()
+lcd.putstr(nic.ifconfig()[0])
+time.sleep(2.0)
+# Doing things
 measure_temperature_and_humidity()
 last_measurement_time = time.time()
 
+# Start Looping
 while True:
     try:
         current_time = time.time()
         if current_time - last_measurement_time > config.MEASUREMENT_INTERVAL: 
             # (Re)Connect to TU Visitor WLAN
-            do_connect_WLAN()
+            if not nic.isconnected():
+                connect_to_wifi()
+            lcd.clear()
+            lcd.putstr(nic.ifconfig()[0])
+            time.sleep(2.0)
             # Doing things
             measure_temperature_and_humidity()
             last_measurement_time = current_time
@@ -103,4 +132,4 @@ while True:
         client.disconnect()
         sys.exit()
     except:
-        pass   
+        continue   
